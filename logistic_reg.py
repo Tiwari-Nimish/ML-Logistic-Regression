4687d129332f6da9b0f526af1272af97786ce1f9
Nimish Tiwari <nktn1605@gmail.com>
2022-08-20 20:07:47 +0530

    Python Files

---
+++ logistic_reg.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+class LogisticRegression:
+    def __init__(self, lr=0.0001,iters=1000):
+        self.lr = lr
+        self.iters = iters
+        self.weights = None
+        self.bias = 0
+
+    def fit(self,x,y):
+        n_samples,n_features = x.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.iters):
+            z = np.dot(x, self.weights) + self.bias
+            y_predicted = 1/(1+np.exp(-z))
+
+            dw = (1/n_samples) * np.dot(x.T,(y_predicted - y))
+            db = (1/n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.lr*dw
+            self.bias -= self.lr*db    
+
+    def predict(self,x):
+        #y_predicted = 1/(1+np.exp(-(np.dot(x,self.weights))))
+        z = np.dot(x, self.weights) + self.bias
+        y_predicted = 1/(1+np.exp(-z))
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
+    
+    def predictedline(self,x):
+        z = np.dot(x, self.weights) + self.bias
+        y_predicted = 1/(1+np.exp(-(z)))
+        return y_predicted
---
+++ logistic_reg_test.py
@@ -0,0 +1,21 @@
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from logistic_reg import LogisticRegression
+import numpy as np
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+bc = datasets.load_breast_cancer()
+X, y = bc.data, bc.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
+
+regressor = LogisticRegression()
+regressor.fit(X_train, y_train)
+predictions = regressor.predict(X_test)
+
+print("LR classification accuracy:", accuracy(y_test, predictions))
